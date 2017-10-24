8fcf4e1d73b5cdae2974ea6cf56d57306b569e08
fenix_63@mail.ru <youran_88>
2017-10-24 22:06:59 +0400

    Добавил разделы для портфолио и контактов

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         if( $(window).scrollTop() > 270 )
         {
             console.log('test');
-            $('.navbar').css({'position':'fixed', 'width':'100%', 'top':'0', 'z-index':'2', 'background':'rgb(90, 165, 245)'});
+            $('.navbar').css({'position':'fixed', 'width':'100%', 'top':'0', 'z-index':'2', 'background':'rgb(130, 187, 249)'});
         }
         else
         {
